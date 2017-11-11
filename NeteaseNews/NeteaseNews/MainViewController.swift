c2898f4c1b984e0caac2cc29cc4d90eb12441c81
ADDICE <addice.ch@gmail.com>
2017-11-11 17:20:26 +0800

    底层改成 SplitViewController

--- NeteaseNews/NeteaseNews/MainViewController.swift
+++ NeteaseNews/NeteaseNews/MainViewController.swift
@@ -14,8 +14,9 @@ class MainViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
